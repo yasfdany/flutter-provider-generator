c1a2850f692ce4ce7860be065049c54bfa6fefd5
Ya'suf Dany <yasfdany@gmail.com>
2024-05-02 00:48:44 +0700

    Generate and write provider template files

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,30 @@
 import * as vscode from 'vscode';
+import { generateTemplate } from './generator';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('flutter-provider-generator.newProviderTemplate', async () => {
+	let disposable = vscode.commands.registerCommand('flutter-provider-generator.newProviderTemplate', async (uri:vscode.Uri) => {
 		const featureName = await vscode.window.showInputBox({
 			placeHolder: "Enter feature name",
 			validateInput(value) {
 				const featureNameRegex = /^[a-z]+(_[a-z]+)*$/;
 
-				if (value && !featureNameRegex.test(value)) {
+				if (value && value.length > 0 && !featureNameRegex.test(value)) {
 					return 'Invalid feature name. Please use snake_case.';
 				}
 				return null;
 			},
 		});
-		
-		vscode.window.showInformationMessage(`${featureName} feature created!`);
+
+		// uri.fsPath
+
+		if(featureName && featureName.length > 0){
+			const success = await generateTemplate(featureName, uri.fsPath);
+			if(!success) {
+				vscode.window.showErrorMessage(`Failed to create ${featureName} feature!`);
+				return;
+			}
+			vscode.window.showInformationMessage(`${featureName} feature created!`);
+		}		
 	});
 
 	context.subscriptions.push(disposable);
